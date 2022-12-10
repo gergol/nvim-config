c107205e243e32f30f0d9ca5ee3c5e9147c31ace
Gerald Golka <golka.gerald@gmail.com>
2022-12-10 12:03:03 +0100

    Added dressing to get lsp actions window

--- lvim/config.lua
+++ lvim/config.lua
@@ -91,104 +91,107 @@ lvim.builtin.which_key.setup.plugins.presets.motions = true
 -- }
 
 lvim.builtin.which_key.mappings["f"] = {
-    name = "+Telescope",
-    w = { function() require("telescope.builtin").live_grep() end, "Search words" },
-    W = {
-        function()
-            require("telescope.builtin").live_grep {
-                additional_args = function(args) return vim.list_extend(args,
-                        { "--hidden", "--no-ignore" })
-                end
-            }
-        end,
-        "Search words in all files",
-    },
-    -- gt = { function() require("telescope.builtin").git_status() end, "Git status" },
-    -- gb = { function() require("telescope.builtin").git_branches() end, "Git branches" },
-    -- gc = { function() require("telescope.builtin").git_commits() end, "Git commits" },
-    f = { function() require("telescope.builtin").git_files() end, "Search files" },
-    F = {
-        function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
-        "Search all files",
-    },
-    b = { function() require("telescope.builtin").buffers() end, "Search buffers" },
-    h = { function() require("telescope.builtin").help_tags() end, "Search help" },
-    m = { function() require("telescope.builtin").marks() end, "Search marks" },
-    o = { function() require("telescope.builtin").oldfiles() end, "Search history" },
-    c =
-    { function() require("telescope.builtin").grep_string() end, "Search for word under cursor" },
-    -- sb = { function() require("telescope.builtin").git_branches() end, "Git branches" },
-    -- sh = { function() require("telescope.builtin").help_tags() end, "Search help" },
-    -- sm = { function() require("telescope.builtin").man_pages() end, "Search man" },
-    -- sr = { function() require("telescope.builtin").registers() end, "Search registers" },
-    -- sk = { function() require("telescope.builtin").keymaps() end, "Search keymaps" },
-    -- sc = { function() require("telescope.builtin").commands() end, "Search commands" },
+  name = "+Telescope",
+  w = { function() require("telescope.builtin").live_grep() end, "Search words" },
+  W = {
+    function()
+      require("telescope.builtin").live_grep {
+        additional_args = function(args) return vim.list_extend(args,
+            { "--hidden", "--no-ignore" })
+        end
+      }
+    end,
+    "Search words in all files",
+  },
+  -- gt = { function() require("telescope.builtin").git_status() end, "Git status" },
+  -- gb = { function() require("telescope.builtin").git_branches() end, "Git branches" },
+  -- gc = { function() require("telescope.builtin").git_commits() end, "Git commits" },
+  f = { function() require("telescope.builtin").git_files() end, "Search files" },
+  F = {
+    function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
+    "Search all files",
+  },
+  b = { function() require("telescope.builtin").buffers() end, "Search buffers" },
+  h = { function() require("telescope.builtin").help_tags() end, "Search help" },
+  m = { function() require("telescope.builtin").marks() end, "Search marks" },
+  o = { function() require("telescope.builtin").oldfiles() end, "Search history" },
+  c =
+  { function() require("telescope.builtin").grep_string() end, "Search for word under cursor" },
+  -- sb = { function() require("telescope.builtin").git_branches() end, "Git branches" },
+  -- sh = { function() require("telescope.builtin").help_tags() end, "Search help" },
+  -- sm = { function() require("telescope.builtin").man_pages() end, "Search man" },
+  -- sr = { function() require("telescope.builtin").registers() end, "Search registers" },
+  -- sk = { function() require("telescope.builtin").keymaps() end, "Search keymaps" },
+  -- sc = { function() require("telescope.builtin").commands() end, "Search commands" },
 }
 
 lvim.builtin.which_key.mappings["b"] = {
-    name = "+Cmake",
-    B = { ":CMake<cr>", "Create CMake project" },
-    b = { ":CMakeBuild<cr>", "Build CMake project" },
-    t = { ":Telescope cmake4vim select_build_type<cr>", "Select CMake build type" },
-    T = { ":Telescope cmake4vim select_target<cr>", "Select CMake target" },
-    k = { ":Telescope cmake4vim select_kit<cr>", "Select CMake kit" },
-    C = { ":CMakeClean<cr>", "Clean project" },
-    s = { ":CMakeCompileSource<cr>", "Compile current buffer" },
-    r = { ":CMakeResetAndReload<cr>", "Reset cmake cache" },
-    R = { ":CMakeReset<cr>", "Reset build folder completely" },
-    i = { ":CMakeInfo<cr>", "Info" }
+  name = "+Cmake",
+  B = { ":CMake<cr>", "Create CMake project" },
+  b = { ":CMakeBuild<cr>", "Build CMake project" },
+  t = { ":Telescope cmake4vim select_build_type<cr>", "Select CMake build type" },
+  T = { ":Telescope cmake4vim select_target<cr>", "Select CMake target" },
+  k = { ":Telescope cmake4vim select_kit<cr>", "Select CMake kit" },
+  C = { ":CMakeClean<cr>", "Clean project" },
+  s = { ":CMakeCompileSource<cr>", "Compile current buffer" },
+  r = { ":CMakeResetAndReload<cr>", "Reset cmake cache" },
+  R = { ":CMakeReset<cr>", "Reset build folder completely" },
+  i = { ":CMakeInfo<cr>", "Info" }
 }
 
+lvim.builtin.which_key.mappings["l"]["R"] = { "<cmd>Telescope lsp_references<cr>", "Find references" }
+lvim.builtin.which_key.mappings["l"]["A"] = { function() vim.lsp.buf.code_action() end, "Range LSP code action" }
+
 lvim.builtin.which_key.mappings["t"] = {
-    name = "+Terminal",
-    j = { ":FloatermToggle<cr>" }
+  name = "+Terminal",
+  j = { ":FloatermToggle<cr>" }
 }
 
 lvim.builtin.which_key.mappings["d"] = {
-    name = "+Debug",
-    d = { ":call vimspector#Launch()<cr>", "Launch debugging" },
-    c = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.code)<cr>",
-        "Focus code",
-    },
-    t = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.terminal)<cr>",
-        "Focus terminal",
-    },
-    v = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.variables)<cr>",
-        "Focus variables",
-    },
-    w = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.watches)<cr>",
-        "Focus watches",
-    },
-    s = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.stack_trace)<cr>",
-        "Fockus stack trace",
-    },
-    o = {
-        ":call GotoWindowNoMax(g:vimspector_session_windows.output)<cr>",
-        "Focus output",
-    },
-    i = {
-        "<Plug>VimspectorBalloonEval",
-        "Evaluate popup", silent = true,
-    },
-    e = { ":call vimspector#Reset()<cr>", "Stop debugging" },
+  name = "+Debug",
+  d = { ":call vimspector#Launch()<cr>", "Launch debugging" },
+  c = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.code)<cr>",
+    "Focus code",
+  },
+  t = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.terminal)<cr>",
+    "Focus terminal",
+  },
+  v = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.variables)<cr>",
+    "Focus variables",
+  },
+  w = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.watches)<cr>",
+    "Focus watches",
+  },
+  s = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.stack_trace)<cr>",
+    "Fockus stack trace",
+  },
+  o = {
+    ":call GotoWindowNoMax(g:vimspector_session_windows.output)<cr>",
+    "Focus output",
+  },
+  i = {
+    "<Plug>VimspectorBalloonEval",
+    "Evaluate popup", silent = true,
+  },
+  e = { ":call vimspector#Reset()<cr>", "Stop debugging" },
 }
 
 lvim.builtin.which_key.mappings["h"] = {
-    name = "+Harpoon",
+  name = "+Harpoon",
 
-    a = { function() require("harpoon.mark").add_file() end, "Add file to menu" },
-    h = {
-        function() require("harpoon.ui").toggle_quick_menu() end,
-        "Show pinned files"
-    },
-    j = { function() require("harpoon.ui").nav_file(1) end, "Go to file 1" },
-    k = { function() require("harpoon.ui").nav_file(2) end, "Go to file 2" },
-    l = { function() require("harpoon.ui").nav_file(3) end, "Go to file 3" },
+  a = { function() require("harpoon.mark").add_file() end, "Add file to menu" },
+  h = {
+    function() require("harpoon.ui").toggle_quick_menu() end,
+    "Show pinned files"
+  },
+  j = { function() require("harpoon.ui").nav_file(1) end, "Go to file 1" },
+  k = { function() require("harpoon.ui").nav_file(2) end, "Go to file 2" },
+  l = { function() require("harpoon.ui").nav_file(3) end, "Go to file 3" },
 }
 
 lvim.builtin.which_key.mappings["h"][";"] = { function() require("harpoon.ui").nav_file(4) end, "Go to file 4" }
@@ -216,10 +219,11 @@ lvim.keys.normal_mode["<C-j>"] = ":cnext<cr>"
 -- Debugger
 
 lvim.keys.normal_mode["<F5>"] = ":call vimspector#Continue()<cr>"
+lvim.keys.normal_mode["<F4>"] = ":call vimspector#Restart()<cr>"
 lvim.keys.normal_mode["<F6>"] = ":call vimspector#Pause()<cr>"
 lvim.keys.normal_mode["<F9>"] = ":call vimspector#ToggleBreakpoint()<cr>"
 lvim.keys.normal_mode["<S-F9>"] =
-":call vimspector#ToggleConditionalBreakpoint()<cr>"
+":call vimspector#ToggleAdvancedBreakpoint()<cr>"
 lvim.keys.normal_mode["<F10>"] = ":call vimspector#StepOver()<cr>"
 lvim.keys.normal_mode["<S-F11>"] = ":call vimspector#StepOut()<cr>"
 lvim.keys.normal_mode["<F11>"] = ":call vimspector#StepInto()<cr>"
@@ -235,27 +239,27 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "cpp",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
+  "bash",
+  "c",
+  "cpp",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
 }
 
 lvim.builtin.lualine.sections.lualine_c = {
-    { "cmake4vim",
-        prefix = "", -- Text to show befor the actual configuration
-        cmake4vim_separator = ">", -- Seperator used between the configuration items
-        colored = true, -- Displays filetype icon in color if set to true
-    }
+  { "cmake4vim",
+    prefix = "", -- Text to show befor the actual configuration
+    cmake4vim_separator = ">", -- Seperator used between the configuration items
+    colored = true, -- Displays filetype icon in color if set to true
+  }
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -263,22 +267,22 @@ lvim.builtin.treesitter.highlight.enable = true
 
 local ts = lvim.builtin.treesitter
 ts.textobjects = {
-    select = {
-        enable = true,
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
-        keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-        },
+  select = {
+    enable = true,
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+    keymaps = {
+      -- You can use the capture groups defined in textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
     },
-    swap = {
-        enable = false,
-        -- swap_next = textobj_swap_keymaps,
-    },
+  },
+  swap = {
+    enable = false,
+    -- swap_next = textobj_swap_keymaps,
+  },
 }
 -- generic LSP settings
 
@@ -303,33 +307,33 @@ ts.textobjects = {
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 require("lvim.lsp.manager").setup("clangd", {
-    cmd = {
-        -- see clangd --help-hidden
-        "clangd",
-        "--background-index",
-        -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-        -- to add more checks, create .clang-tidy file in the root directory
-        -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-        "--clang-tidy",
-        "--completion-style=bundled",
-        "--cross-file-rename",
-        "--header-insertion=iwyu",
-        "--header-insertion-decorators",
-        "--pch-storage=memory",
-        "--suggest-missing-includes",
-        -- "--malloc-trim",
-        --    "--all-scopes-completion",
-        "-j=6",
-        "--offset-encoding=utf-16"
-        --    "--log=verbose",
-        --    "--pretty",
-    },
-    init_options = {
-        clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
-        usePlaceholders = true,
-        completeUnimported = true,
-        semanticHighlighting = true,
-    },
+  cmd = {
+    -- see clangd --help-hidden
+    "clangd",
+    "--background-index",
+    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+    -- to add more checks, create .clang-tidy file in the root directory
+    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
+    "--clang-tidy",
+    "--completion-style=bundled",
+    "--cross-file-rename",
+    "--header-insertion=iwyu",
+    "--header-insertion-decorators",
+    "--pch-storage=memory",
+    "--suggest-missing-includes",
+    -- "--malloc-trim",
+    --    "--all-scopes-completion",
+    "-j=6",
+    "--offset-encoding=utf-16"
+    --    "--log=verbose",
+    --    "--pretty",
+  },
+  init_options = {
+    clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
+    usePlaceholders = true,
+    completeUnimported = true,
+    semanticHighlighting = true,
+  },
 })
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -386,41 +390,49 @@ lvim.builtin.dap.active = false
 
 -- Additional Plugins
 lvim.plugins = {
-    --     {
-    --       "folke/trouble.nvim",
-    --       cmd = "TroubleToggle",
-    --     },
-    { 'ThePrimeagen/vim-be-good' },
-    { "windwp/nvim-ts-autotag" }, { "tpope/vim-abolish" },
-    { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    -- {
-    --     "zbirenbaum/copilot.lua",
-    --     event = { "VimEnter" },
-    --     config = function()
-    --         vim.defer_fn(function()
-    --             require("copilot").setup {
-    --                 plugin_manager_path = os.getenv "LUNARVIM_RUNTIME_DIR" .. "/site/pack/packer",
-    --             }
-    --         end, 100)
-    --     end,
-    -- },
-    -- {
-    --     "zbirenbaum/copilot-cmp",
-    --     after = { "copilot.lua" },
-    --     config = function()
-    --         require("copilot_cmp").setup()
-    --     end,
-    -- },
-    { "github/copilot.vim" },
-    { "ThePrimeagen/harpoon" },
-    { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    {
-        "ilyachur/cmake4vim",
-        requires = { "SantinoKeupp/telescope-cmake4vim.nvim", "SantinoKeupp/lualine-cmake4vim.nvim" },
-        config = function() require("user.plugins.cmake4vim").setup() end,
-    },
-    { "puremourning/vimspector", setup = function()
-        vim.cmd [[
+  --     {
+  --       "folke/trouble.nvim",
+  --       cmd = "TroubleToggle",
+  --     },
+  { 'ThePrimeagen/vim-be-good' },
+  { "windwp/nvim-ts-autotag" }, { "tpope/vim-abolish" },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  {
+    "stevearc/dressing.nvim",
+    config = function()
+      require("user.plugins.dress").config()
+    end,
+    disable = false,
+    event = "BufWinEnter",
+  },
+  -- {
+  --     "zbirenbaum/copilot.lua",
+  --     event = { "VimEnter" },
+  --     config = function()
+  --         vim.defer_fn(function()
+  --             require("copilot").setup {
+  --                 plugin_manager_path = os.getenv "LUNARVIM_RUNTIME_DIR" .. "/site/pack/packer",
+  --             }
+  --         end, 100)
+  --     end,
+  -- },
+  -- {
+  --     "zbirenbaum/copilot-cmp",
+  --     after = { "copilot.lua" },
+  --     config = function()
+  --         require("copilot_cmp").setup()
+  --     end,
+  -- },
+  { "github/copilot.vim" },
+  { "ThePrimeagen/harpoon" },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  {
+    "ilyachur/cmake4vim",
+    requires = { "SantinoKeupp/telescope-cmake4vim.nvim", "SantinoKeupp/lualine-cmake4vim.nvim" },
+    config = function() require("user.plugins.cmake4vim").setup() end,
+  },
+  { "puremourning/vimspector", setup = function()
+    vim.cmd [[
 let g:vimspector_install_gadgets = [ 'debugpy', 'vscode-cpptools', 'CodeLLDB' ]
 
 " let g:vimspector_enable_mappings = 'HUMAN'
@@ -443,7 +455,7 @@ let g:vimspector_terminal_minwidth = 20
 let g:vimspector_sidebar_width = 100
 let g:vimspector_bottombar_height = 40
 ]]
-    end },
+  end },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
