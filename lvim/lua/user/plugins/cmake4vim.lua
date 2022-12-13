2bb10c85fe66c4285e4703848088011b3b34e50c
Gerald Golka <golka.gerald@gmail.com>
2022-12-13 10:19:09 +0100

    Changed cmake build folder

--- lvim/lua/user/plugins/cmake4vim.lua
+++ lvim/lua/user/plugins/cmake4vim.lua
@@ -1,9 +1,13 @@
-return {    
-setup = function()
-        vim.cmd "let g:cmake_reload_after_save = 1"
-        vim.cmd "let g:cmake_compile_commands = 1"
-        vim.cmd "let g:cmake_compile_commands_link='.'"
-        vim.cmd [[ let g:cmake_kits = {
+return {
+  setup = function()
+    vim.cmd [[let g:cmake_build_dir="./build"]]
+    vim.cmd [[let g:cmake_build_type="Debug"]]
+    vim.cmd [[let g:cmake_ctest_args="-j8 --output-on-failure"]]
+    vim.cmd [[let g:cmake_vimspector_support=1]]
+    vim.cmd "let g:cmake_reload_after_save = 1"
+    vim.cmd "let g:cmake_compile_commands = 1"
+    vim.cmd "let g:cmake_compile_commands_link='.'"
+    vim.cmd [[ let g:cmake_kits = {
             \  "gcc": {
             \    "compilers": {
             \      "C": "/usr/bin/gcc",
@@ -20,5 +24,5 @@ setup = function()
             \  }
             \}
             ]]
-    end,
+  end,
 }
