bf65700abcbf5b36309be5ab8bf5866db4be330b
Gerald Golka <golka.gerald@gmail.com>
2023-07-14 10:37:40 +0200

    small null-ls adjustments

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -19,18 +19,18 @@ local M = {
     -- before we configure null-ls, to use mason-null-ls
     -- as a source of truth. So we handle the setup order here.
     require("mason-null-ls").setup({
-        ensure_installed = {
-            'black',
-            -- flake8 required_plugins can be installed via:
-            -- - `~/.local/share/nvim/mason/packages/flake8/venv/bin/pip3 install flake8-bugbear`
-            'flake8',
-            -- Typeshed packages can be installed for mypy via:
-            -- `~/.local/share/nvim/mason/packages/mypy/venv/bin/pip3 install -r requirements.txt`
-            'mypy@0.971',
-            'shellcheck',
-        },
-        automatic_installation = false,
-        automatic_setup = false,
+      ensure_installed = {
+        'black',
+        -- flake8 required_plugins can be installed via:
+        -- - `~/.local/share/nvim/mason/packages/flake8/venv/bin/pip3 install flake8-bugbear`
+        'flake8',
+        -- Typeshed packages can be installed for mypy via:
+        -- `~/.local/share/nvim/mason/packages/mypy/venv/bin/pip3 install -r requirements.txt`
+        'mypy@0.971',
+        'shellcheck',
+      },
+      automatic_installation = false,
+      automatic_setup = false,
     })
 
     local ls = require('null-ls')
@@ -39,12 +39,20 @@ local M = {
       diagnostics_format = "#{m} #{s}[#{c}]",
       sources = {
         ls.builtins.formatting.black.with({
-          extra_args = {"--line-length=120", "--skip-string-normalization"},
+          extra_args = { "--line-length=120" },
         }),
-        ls.builtins.diagnostics.flake8,
+        ls.builtins.diagnostics.flake8.with({
+          extra_args = { "--max-line-length=120" },
+        }),
         ls.builtins.diagnostics.mypy.with({
           -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1208#issuecomment-1343562820
-          cwd = function (_) return vim.fn.getcwd() end,
+          cwd = function(_) return vim.fn.getcwd() end,
+          -- detect virtualenv or fall back to default python
+          -- see: https://stackoverflow.com/a/76487663/8494588
+          extra_args = function()
+            local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_DEFAULT_ENV") or "/usr"
+            return { "--python-executable", virtual .. "/bin/python3" }
+          end,
         }),
         ls.builtins.diagnostics.shellcheck,
       },
