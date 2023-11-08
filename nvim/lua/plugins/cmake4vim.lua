30391d60eed451a7569634db94e2de1eaee34b6a
Gerald Golka <golka.gerald@gmail.com>
2023-11-08 10:35:25 +0100

    Prune vimspector

--- nvim/lua/plugins/cmake4vim.lua
+++ nvim/lua/plugins/cmake4vim.lua
@@ -5,7 +5,6 @@ return {
     vim.cmd [[let g:cmake_build_dir="./build"]]
     vim.cmd [[let g:cmake_build_type="Debug"]]
     vim.cmd [[let g:cmake_ctest_args="-j8 --output-on-failure"]]
-    vim.cmd [[let g:cmake_vimspector_support=1]]
     vim.cmd "let g:cmake_reload_after_save = 0"
     vim.cmd "let g:cmake_compile_commands = 1"
     vim.cmd "let g:cmake_compile_commands_link='.'"
