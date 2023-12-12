43efd63bac3011f4e7ff18af6f51f124e29a3ff4
Gerald Golka <golka.gerald@gmail.com>
2023-12-12 13:02:32 +0100

    updated install script

--- nvim/install_nvim.sh
+++ nvim/install_nvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# This script unattendedly installs neovim and configures it.
+#
 
 function install_nvim() {
   if ! command -v "nvim" >/dev/null 2>&1; then
@@ -55,6 +57,18 @@ function clone_or_update_nvim_config() {
   fi
 }
 
+function install_lazygit() {
+    if ! command -v "lazygit" >/dev/null 2>&1; then
+        echo >&2 "Installing lazygit"
+            LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+            curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+            tar xf lazygit.tar.gz lazygit
+            sudo install lazygit /usr/local/bin   
+    else
+        return
+    fi
+}
+
 function install_virtualenv() {
     check_and_install pip3 python3-pip
     if ! which virtualenvwrapper.sh >/dev/null 2>&1; then
@@ -80,6 +94,16 @@ function install_virtualenv() {
       fi
 }
 
+function check_and_install_python_package() {
+    package_name=$1
+    if ! pip3 show "$package_name" > /dev/null 2>&1; then
+        echo "Installing $package_name..."
+        pip3 install "$package_name"
+    else
+        echo "$package_name is already installed."
+    fi
+}
+
 function check_and_activate_or_create_venv() {
     if lsvirtualenv | grep -q "$1"; then
         workon "$1"
@@ -97,9 +121,10 @@ check_and_install fzf
 
 install_nvim
 install_git
+install_lazygit
 install_virtualenv
 check_and_activate_or_create_venv neovim
-pip3 install neovim
+check_and_install_python_package neovim
 clone_or_update_nvim_config
 
 
