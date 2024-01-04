db2f1b3dc8274ff7e7103c77d7661cdf7d711203
Gerald Golka <golka.gerald@gmail.com>
2024-01-04 16:58:40 +0100

    disabled sonarlint because it causes too much clutter and trouble

--- nvim/lua/plugins/sonarlint-ls.lua
+++ nvim/lua/plugins/sonarlint-ls.lua
@@ -1,30 +1,30 @@
-return {
-   'schrieveslaach/sonarlint.nvim',
-   url = 'https://gitlab.com/schrieveslaach/sonarlint.nvim',
-   dependencies = {
-      'neovim/nvim-lspconfig',
-   },
-   config = function()
-      require('sonarlint').setup({
-         server = {
-            cmd = {
-               'sonarlint-language-server',
-               -- Ensure that sonarlint-language-server uses stdio channel
-               '-stdio',
-               '-analyzers',
-               -- paths to the analyzers you need, using those for python and java in this example
-               vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-               vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-               -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-            }
-         },
-         filetypes = {
-            -- Tested and working
-            -- 'python',
-            'cpp',
-            -- Requires nvim-jdtls, otherwise an error message will be printed
-            -- 'java',
-         }
-      })
-   end,
-}
+-- return {
+--    'schrieveslaach/sonarlint.nvim',
+--    url = 'https://gitlab.com/schrieveslaach/sonarlint.nvim',
+--    dependencies = {
+--       'neovim/nvim-lspconfig',
+--    },
+--    config = function()
+--       require('sonarlint').setup({
+--          server = {
+--             cmd = {
+--                'sonarlint-language-server',
+--                -- Ensure that sonarlint-language-server uses stdio channel
+--                '-stdio',
+--                '-analyzers',
+--                -- paths to the analyzers you need, using those for python and java in this example
+--                vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+--                vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+--                -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+--             }
+--          },
+--          filetypes = {
+--             -- Tested and working
+--             -- 'python',
+--             'cpp',
+--             -- Requires nvim-jdtls, otherwise an error message will be printed
+--             -- 'java',
+--          }
+--       })
+--    end,
+-- }
