ee48d0c68106c8d9fc95c210d9d0e07f564594f2
Gerald Golka <golka.gerald@gmail.com>
2024-01-12 11:46:45 +0100

    switched to catpuccin theme

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -1,14 +1,15 @@
-return { -- Theme inspired by Atom
-  'navarasu/onedark.nvim',
-  priority = 1000,
-  config = function()
-    require("onedark").setup {
-      on_highlights = function(hl, c)
-        hl.CursorLine = {
-          bg = "#ffffff",
-        }
-      end,
-    }
-    vim.cmd.colorscheme 'onedark'
-  end,
-}
+return {}
+-- return { -- Theme inspired by Atom
+--   'navarasu/onedark.nvim',
+--   priority = 1000,
+--   config = function()
+--     require("onedark").setup {
+--       on_highlights = function(hl, c)
+--         hl.CursorLine = {
+--           bg = "#ffffff",
+--         }
+--       end,
+--     }
+--     vim.cmd.colorscheme 'onedark'
+--   end,
+-- }
