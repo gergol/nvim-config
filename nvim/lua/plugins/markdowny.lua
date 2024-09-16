0f1985c8dd49418c0229402a7fd77f440ca41b79
Gerald Golka <golka.gerald@gmail.com>
2024-09-16 10:35:12 +0200

    more key bind refactoring

--- nvim/lua/plugins/markdowny.lua
+++ nvim/lua/plugins/markdowny.lua
@@ -6,17 +6,28 @@ return {
       pattern = 'markdown',
       callback = function()
         local wk = require('which-key')
-        wk.register({
-          m = {
-            name = "+Markdown",
-            p = { ":MarkdownPreviewToggle<cr>", "Preview markdown on/off", mode = "n" },
-            b = { ":lua require('markdowny').bold()<cr>", "Bold", mode = "v" },
-            i = { ":lua require('markdowny').italic()<cr>", "Italic", mode = "v" },
-            k = { ":lua require('markdowny').link()<cr>", "Link", mode = "v" },
-            c = { ":lua require('markdowny').code()<cr>", "Code", mode = "v" },
-
-          }
-        }, { prefix = "<leader>", buffer = 0, mode = {'n', 'v'} })
+        wk.add({
+          {
+            mode = { "v" },
+            { "<leader>mb", ":lua require('markdowny').bold()<cr>",   buffer = 0, desc = "Bold" },
+            { "<leader>mc", ":lua require('markdowny').code()<cr>",   buffer = 0, desc = "Code" },
+            { "<leader>mi", ":lua require('markdowny').italic()<cr>", buffer = 0, desc = "Italic" },
+            { "<leader>mk", ":lua require('markdowny').link()<cr>",   buffer = 0, desc = "Link" },
+          },
+          { "<leader>m",  buffer = 0,                   group = "Markdown", mode = { "n", "v" } },
+          { "<leader>mp", ":MarkdownPreviewToggle<cr>", buffer = 0,         desc = "Preview markdown on/off" },
+        })
+        -- wk.register({
+        --   m = {
+        --     name = "+Markdown",
+        --     p = { ":MarkdownPreviewToggle<cr>", "Preview markdown on/off", mode = "n" },
+        --     b = { ":lua require('markdowny').bold()<cr>", "Bold", mode = "v" },
+        --     i = { ":lua require('markdowny').italic()<cr>", "Italic", mode = "v" },
+        --     k = { ":lua require('markdowny').link()<cr>", "Link", mode = "v" },
+        --     c = { ":lua require('markdowny').code()<cr>", "Code", mode = "v" },
+        --
+        --   }
+        -- }, { prefix = "<leader>", buffer = 0, mode = {'n', 'v'} })
         -- vim.keymap.set('v', '<leader>mb', ":lua require('markdowny').bold()<cr>", { buffer = 0, desc="Bold" })
         -- vim.keymap.set('v', '<leader>mi', ":lua require('markdowny').italic()<cr>", { buffer = 0, desc="Italic" })
         -- vim.keymap.set('v', '<leader>mk', ":lua require('markdowny').link()<cr>", { buffer = 0, desc="Link" })
