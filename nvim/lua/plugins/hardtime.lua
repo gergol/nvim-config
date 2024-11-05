e03bf2d1af9c054fc78519599bac70833bdb903d
Gerald Golka <golka.gerald@gmail.com>
2024-11-05 09:51:51 +0100

    disabled hardtime pluging

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -1,8 +1,9 @@
-return {
-  "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {
-    -- Add "oil" to the disabled_filetypes
-    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" }
-  }
-}
+return {}
+-- return {
+--   "m4xshen/hardtime.nvim",
+--   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+--   opts = {
+--     -- Add "oil" to the disabled_filetypes
+--     disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" }
+--   }
+-- }
