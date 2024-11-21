a644ddfcf37234b87559d5f8dee8786c930fe456
Gerald Golka <golka.gerald@gmail.com>
2024-11-21 10:42:56 +0100

    added codecompanion

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,31 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp",                                                                    -- Optional: For using slash commands and variables in the chat buffer
+    "nvim-telescope/telescope.nvim",                                                       -- Optional: For using slash commands
+    { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+    { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
+  },
+  config = function()
+    require("codecompanion").setup({
+      adapters = {
+        ollama = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              -- url = "http[s]://open_compatible_ai_url", -- optional: default value is ollama url http://127.0.0.1:11434
+              -- api_key = "OpenAI_API_KEY",             -- optional: if your endpoint is authenticated
+              -- chat_url = "/v1/chat/completions",      -- optional: default value, override if different
+            },
+            schema = {
+              model = {
+                default = "llama3.2",
+              },
+            },
+          })
+        end,
+      },
+    })
+  end
+}
