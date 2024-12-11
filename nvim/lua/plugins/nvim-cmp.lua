a74edd2e68153be3d1b872aeb40f257945903618
Gerald Golka <golka.gerald@gmail.com>
2024-12-11 10:11:48 +0100

    added lua snip helper plugin to populate snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,13 +1,15 @@
 return {
   -- Autocompletion
   'hrsh7th/nvim-cmp',
-  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'FelipeLema/cmp-async-path' },
+  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'FelipeLema/cmp-async-path', "rafamadriz/friendly-snippets" },
 
   config = function()
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
 
-    luasnip.config.setup {}
+    luasnip.config.setup {
+      require("luasnip.loaders.from_vscode").lazy_load()
+    }
 
     cmp.setup {
       snippet = {
