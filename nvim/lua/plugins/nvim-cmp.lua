c1b83e9c7563e6e9f1ed6aba05b5e3453358c85c
Gerald Golka <golka.gerald@gmail.com>
2024-12-11 10:53:16 +0100

    added custom snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -8,7 +8,8 @@ return {
     local luasnip = require 'luasnip'
 
     luasnip.config.setup {
-      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load(),
+      require("luasnip.loaders.from_vscode").lazy_load({paths = {"./snippets"}})
     }
 
     cmp.setup {
