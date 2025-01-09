4c93994281a92df3493b18d05deb267e53f34ee9
Gerald Golka <golka.gerald@gmail.com>
2025-01-09 11:29:13 +0100

    Improved code companion

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -9,29 +9,35 @@ return {
     { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
     { 'echasnovski/mini.diff',                     version = '*' },
   },
-  config = function()
-    require("codecompanion").setup({
-      display = {
-        diff = {
-          provider = "mini_diff",
+  opts = {
+    display = {
+      diff = {
+        provider = "mini_diff",
+      },
+      chat = {
+        window = {
+          position = "right",
         },
       },
-      adapters = {
-        ollama = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            env = {
-              -- url = "http[s]://open_compatible_ai_url", -- optional: default value is ollama url http://127.0.0.1:11434
-              -- api_key = "OpenAI_API_KEY",             -- optional: if your endpoint is authenticated
-              -- chat_url = "/v1/chat/completions",      -- optional: default value, override if different
+    },
+    opts = {
+      log_level = "DEBUG",
+    },
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          env = {
+            -- url = "http[s]://open_compatible_ai_url", -- optional: default value is ollama url http://127.0.0.1:11434
+            -- api_key = "OpenAI_API_KEY",             -- optional: if your endpoint is authenticated
+            -- chat_url = "/v1/chat/completions",      -- optional: default value, override if different
+          },
+          schema = {
+            model = {
+              default = "qwen2.5-coder:32b",
             },
-            schema = {
-              model = {
-                default = "qwen2.5-coder:32b",
-              },
-            },
-          })
-        end,
-      },
-    })
-  end
+          },
+        })
+      end,
+    },
+  }
 }
---
+++ nvim/lua/plugins/mini-diff.lua
@@ -0,0 +1,5 @@
+return {
+  'echasnovski/mini.diff',
+  version = '*',
+  config = function() require('mini.diff').setup() end,
+}
