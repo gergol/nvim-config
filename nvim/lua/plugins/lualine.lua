10a21dcc5cec21a723db500b4eff2c4da527796b
Gerald Golka <golka.gerald@gmail.com>
2025-01-27 08:52:32 +0100

    visual feedback on recording macro

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,22 @@
 return { -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'onedark',
+      component_separators = '|',
+      section_separators = '',
+    },
+    sections = {
+      -- helps displaying macro recording. not super necessary though
+      lualine_x = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        }
       },
     },
-  }
+  },
+}
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -27,7 +27,7 @@ return {
       command_palette = true,       -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true,       -- add a border to hover docs and signature help
+      lsp_doc_border = true,        -- add a border to hover docs and signature help
     },
     cmdline = {
       enabled = true,         -- enables the Noice cmdline UI
@@ -38,6 +38,12 @@ return {
           col = "50%",
         }
       },
-    }
+    },
+    routes = {
+      {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+      },
+    },
   }
 }
