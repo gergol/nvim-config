d07d0eb55443cb5c77becb34f24285c08ad85b67
Gerald Golka <golka.gerald@gmail.com>
2025-02-13 09:15:52 +0100

    removed discontinued dressing.nvim

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -6,7 +6,7 @@ return {
     "hrsh7th/nvim-cmp",                                                                    -- Optional: For using slash commands and variables in the chat buffer
     "nvim-telescope/telescope.nvim",                                                       -- Optional: For using slash commands
     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
-    { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
+    -- { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
     { 'echasnovski/mini.diff',                     version = '*' },
   },
   opts = {
