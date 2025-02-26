ff133b57894c02b8bebce928b12830e38bd124c1
Gerald Golka <golka.gerald@gmail.com>
2025-02-26 09:39:42 +0100

    codecompanion: fixed not working

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -23,14 +23,18 @@ return {
     opts = {
       log_level = "DEBUG",
     },
+    strategies = {
+      chat = {
+        adapter = "qwen25",
+      },
+      inline = {
+        adapter = "qwen25",
+      },
+    },
     adapters = {
-      ollama = function()
+      qwen25 = function()
         return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            -- url = "http[s]://open_compatible_ai_url", -- optional: default value is ollama url http://127.0.0.1:11434
-            -- api_key = "OpenAI_API_KEY",             -- optional: if your endpoint is authenticated
-            -- chat_url = "/v1/chat/completions",      -- optional: default value, override if different
-          },
+          name = "qwen25",
           schema = {
             model = {
               default = "qwen2.5-coder:32b",
