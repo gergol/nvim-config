66f3ba7ffb7faab46fbc74a301187da36bf10d1e
Gerald Golka <golka.gerald@gmail.com>
2025-04-08 14:34:20 +0200

    switched to avante

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,47 +1,48 @@
-return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "hrsh7th/nvim-cmp",                                                                    -- Optional: For using slash commands and variables in the chat buffer
-    "nvim-telescope/telescope.nvim",                                                       -- Optional: For using slash commands
-    { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
-    -- { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
-    { 'echasnovski/mini.diff',                     version = '*' },
-  },
-  opts = {
-    display = {
-      diff = {
-        provider = "mini_diff",
-      },
-      chat = {
-        window = {
-          position = "right",
-        },
-      },
-    },
-    opts = {
-      log_level = "DEBUG",
-    },
-    strategies = {
-      chat = {
-        adapter = "qwen25",
-      },
-      inline = {
-        adapter = "qwen25",
-      },
-    },
-    adapters = {
-      qwen25 = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          name = "qwen25",
-          schema = {
-            model = {
-              default = "qwen2.5-coder:32b",
-            },
-          },
-        })
-      end,
-    },
-  }
-}
+return {}
+-- return {
+--   "olimorris/codecompanion.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--     "hrsh7th/nvim-cmp",                                                                    -- Optional: For using slash commands and variables in the chat buffer
+--     "nvim-telescope/telescope.nvim",                                                       -- Optional: For using slash commands
+--     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+--     -- { "stevearc/dressing.nvim",                    opts = {} },                            -- Optional: Improves `vim.ui.select`
+--     { 'echasnovski/mini.diff',                     version = '*' },
+--   },
+--   opts = {
+--     display = {
+--       diff = {
+--         provider = "mini_diff",
+--       },
+--       chat = {
+--         window = {
+--           position = "right",
+--         },
+--       },
+--     },
+--     opts = {
+--       log_level = "DEBUG",
+--     },
+--     strategies = {
+--       chat = {
+--         adapter = "qwen25",
+--       },
+--       inline = {
+--         adapter = "qwen25",
+--       },
+--     },
+--     adapters = {
+--       qwen25 = function()
+--         return require("codecompanion.adapters").extend("openai_compatible", {
+--           name = "qwen25",
+--           schema = {
+--             model = {
+--               default = "qwen2.5-coder:32b",
+--             },
+--           },
+--         })
+--       end,
+--     },
+--   }
+-- }
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,17 +2,6 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  },
-  config = {
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
@@ -45,5 +34,13 @@ return {
         filter = { event = "msg_showmode" },
       },
     },
-  }
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
 }
