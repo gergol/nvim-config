1903c97e8cc1b5bccc70953a80fd28ecc75dbdcf
Gerald Golka <golka.gerald@gmail.com>
2025-06-20 11:54:54 +0200

    chenge conflikting key bind in treesitter (and fix classs selection mode)

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,21 +3,20 @@ return { -- Highlight, edit, and navigate code
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  build = ":TSUpdate",
+  build = ':TSUpdate',
   config = function()
     -- [[ Configure Treesitter ]]
     -- See `:help nvim-treesitter`
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'html',
-        'json', 'markdown' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'html', 'json', 'markdown' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = false,
 
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { "markdown" },
+        additional_vim_regex_highlighting = { 'markdown' },
       },
       indent = { enable = true, disable = { 'python' } },
       incremental_selection = {
@@ -38,16 +37,16 @@ return { -- Highlight, edit, and navigate code
 
           keymaps = {
             -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ia"] = "@parameter.inner",
-            ["aa"] = "@parameter.outer",
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ia'] = '@parameter.inner',
+            ['aa'] = '@parameter.outer',
             -- You can optionally set descriptions to the mappings (used in the desc parameter of
             -- nvim_buf_set_keymap) which plugins like which-key display
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
             -- You can also use captures from other query groups like `locals.scm`
-            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
           },
           -- You can choose the select mode (default is charwise 'v')
           --
@@ -58,8 +57,8 @@ return { -- Highlight, edit, and navigate code
           -- mapping query_strings to modes.
           selection_modes = {
             ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V',  -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@class.outer'] = 'V', -- blockwise
           },
           -- If you set this to `true` (default is `false`) then any textobject is
           -- extended to include preceding or succeeding whitespace. Succeeding
@@ -76,36 +75,36 @@ return { -- Highlight, edit, and navigate code
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+            [']m'] = '@function.outer',
+            [']C'] = { query = '@class.outer', desc = 'Next class start' },
             --
             -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-            ["]o"] = "@loop.*",
+            [']o'] = '@loop.*',
             -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
             --
             -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
             -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-            ["]a"] = "@parameter.inner",
+            [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+            [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+            [']a'] = '@parameter.inner',
           },
           goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-            ["]A"] = "@parameter.outer",
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+            [']A'] = '@parameter.outer',
           },
           goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[o"] = "@loop.*",
-            ["[s"] = { query = "@scope", query_group = "locals", desc = "Prev scope" },
-            ["[z"] = { query = "@fold", query_group = "folds", desc = "Prev fold" },
-            ["[a"] = "@parameter.inner",
+            ['[m'] = '@function.outer',
+            ['[c'] = '@class.outer',
+            ['[o'] = '@loop.*',
+            ['[s'] = { query = '@scope', query_group = 'locals', desc = 'Prev scope' },
+            ['[z'] = { query = '@fold', query_group = 'folds', desc = 'Prev fold' },
+            ['[a'] = '@parameter.inner',
           },
           goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-            ["[A"] = "@parameter.outer",
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+            ['[A'] = '@parameter.outer',
           },
           -- Below will go to either the start or the end, whichever is closer.
           -- Use if you want more granular movements
@@ -128,5 +127,5 @@ return { -- Highlight, edit, and navigate code
         },
       },
     }
-  end
+  end,
 }
