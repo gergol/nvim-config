a8e91f18d731ae61ca6ee4b0b622fa73888c1ea5
Gerald Golka <golka.gerald@gmail.com>
2025-11-07 09:27:47 +0100

    make debugging recognize python / pygind in config gile

--- nvim/lua/plugins/mason-nvim-dap.lua
+++ nvim/lua/plugins/mason-nvim-dap.lua
@@ -1,29 +1,30 @@
 return {
-  "jay-babu/mason-nvim-dap.nvim",
-  event        = "VeryLazy",
+  'jay-babu/mason-nvim-dap.nvim',
+  event = 'VeryLazy',
   -- cmd          = { "DapInstall", "DapUninstall" },
   dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap",
+    'williamboman/mason.nvim',
+    'mfussenegger/nvim-dap',
   },
-  opts         = {
+  opts = {
     automatic_installation = true,
     handlers = {
       function(config)
         require('mason-nvim-dap').default_setup(config)
         -- in original vscode/launch.json the codelldb adapter is named lldb, so we need to copy the configuration over.
-        local dap = require('dap')
+        local dap = require 'dap'
         dap.adapters.lldb = dap.adapters.codelldb
+        dap.adapters.debugpy = dap.adapters.python
         -- same as above but for dbugpy / python
-        -- dap.adapters.python = dap.adapters.debugpy
-        -- print("Loaded dap config")
+        print 'Loaded dap config'
+        print(dap.adapters.debugpy)
         -- print(vim.inspect(config))
       end,
     }, -- load default handlers
     ensure_installed = {
-      "python",
-      "vscode-cpptools",
-      "codelldb",
+      'python',
+      'vscode-cpptools',
+      'codelldb',
     },
   },
 }
